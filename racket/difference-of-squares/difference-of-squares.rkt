557cfa2ab15f5b098e16e70d4f9dbfe0c2bed1bf
heycalmdown <heyacct@gmail.com>
2019-04-19 13:41:17 +0900

    racket) difference-of-squares - abstract the domain, rather than the language

--- racket/difference-of-squares/difference-of-squares.rkt
+++ racket/difference-of-squares/difference-of-squares.rkt
@@ -2,40 +2,22 @@
 
 (provide sum-of-squares square-of-sum difference)
 
-(define/contract (first-n-natural-numbers n)
-  (-> exact-positive-integer? (stream/c exact-positive-integer?))
-
-  "The first N natural numbers"
-
-  (in-range 1 (add1 n)))
-
-(define/contract (sum-of l)
-  (-> (stream/c exact-positive-integer?) exact-positive-integer?)
-
-  "The sum of given list"
-
-  (for/sum ([i l]) i))
-
-(define/contract (squares-of seq-of-n)
-  (-> (stream/c exact-positive-integer?) (stream/c exact-positive-integer?))
-
-  "The squares of sequence of natural numbers"
-
-  (for/list ([i seq-of-n]) (sqr i)))
-
 (define/contract (sum-of-squares n)
   (-> exact-positive-integer? exact-positive-integer?)
 
   "The sum of the squares of the first N natural numbers"
 
-  (sum-of (squares-of (first-n-natural-numbers n))))
+  (for/sum
+      ([i
+        (for/list ([i n]) (sqr (add1 i)))])
+    i))
 
 (define/contract (square-of-sum n)
   (-> exact-positive-integer? exact-positive-integer?)
 
   "The square of the sum of the first N natural numbers"
 
-  (sqr (sum-of (first-n-natural-numbers n))))
+  (sqr (for/sum ([i n]) (add1 i))))
 
 (define/contract (difference n)
   (-> exact-positive-integer? exact-positive-integer?)
